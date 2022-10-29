170b67fa7536995e9bf7afcc220ec9661d5834ba
qwezs <qwezs@DESKTOP-5HC4037>
2022-10-30 02:57:32 +0300

    Fix

--- Prakt7/Rectangle.cs
+++ Prakt7/Rectangle.cs
@@ -8,14 +8,23 @@ namespace Prakt7
 {
             class Rectangle : Pair
         {
-
+        /// <summary>
+        /// Операция нахождения площади прямоугольника S=a*b
+        /// </summary>
+        /// <param name="value1">Значение сторон прямоугольника(a,b)</param>
+        /// <returns>Возвращаемое значение result результать операции нахождение площади</returns>
             public Rectangle Sqare(Rectangle value1)
             {
                 Rectangle result = new Rectangle();
                 result.para1 = value1.Para1 * value1.Para2;
                 return result;
             }
-            public Rectangle RectP(Rectangle value1)
+        /// <summary>
+        /// Операция нахождения периметра прямоугольника P=2(a*b)
+        /// </summary>
+        /// <param name="value1">Значение сторон прямоугольника(a,b)</param>
+        /// <returns>Возвращаемое значение result результать операции нахождение периметра</returns>
+        public Rectangle RectP(Rectangle value1)
             {
                 Rectangle result = new Rectangle();
                 result.para1 = 2 * (value1.Para1 * value1.Para2);
